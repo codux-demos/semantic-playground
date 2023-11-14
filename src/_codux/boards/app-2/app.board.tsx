f18e57239bf5be14449ef000fcb5ca3a0022a5eb
Sagiv Dayan <sagiv.keeper@gmail.com>
2023-11-14 17:17:04 +0200

    chore: create duplicate with more correct css

--- src/_codux/boards/app-2/app.board.tsx
+++ src/_codux/boards/app-2/app.board.tsx
@@ -1,11 +1,8 @@
 import { createBoard } from '@wixc3/react-board';
-import App from '../../../App';
+import App from '../../../App2';
 
 export default createBoard({
     name: 'App',
     Board: () => <App />,
     isSnippet: true,
-    environmentProps: {
-        windowHeight: 670
-    }
 });
